b9c471a4c4f24edfaed6b55d72f3597c26bec2f9
Robert Salvo <salvo.robertroy@gmail.com>
2025-11-01 21:16:51 +0800

    chore: edited notify controller

--- src/server/controllers/notify.controller.ts
+++ src/server/controllers/notify.controller.ts
@@ -1,26 +1,29 @@
-import { db } from "@/server/firebase/config"
+import { db } from "@/server/firebase/config";
 import { sendEmailService } from "@/server/services/nodemailer.service";
-import { addDoc, collection, serverTimestamp } from "firebase/firestore"
+import { addDoc, collection, query, where, getDocs, serverTimestamp } from "firebase/firestore";
 
 const sendEmail = async (email: string) => {
   try {
+    const notifyRef = collection(db, "emails");
 
-    const notifyRef = collection(db, 'emails');
-    await addDoc(notifyRef, {
+    const q = query(notifyRef, where("email", "==", email));
+    const snapshot = await getDocs(q);
+
+    if (snapshot.empty) {
+      await addDoc(notifyRef, {
         email,
-        createdAt: serverTimestamp()
-      }
-    )
+        createdAt: serverTimestamp(),
+      });
+    }
 
     await sendEmailService(email);
 
-    return { message: 'Email saved and notification sent successfully'};
-
+    return { message: "Email saved and notification sent successfully." };
   } catch (error: any) {
     console.error("Error in sendEmail controller:", error);
-    throw new Error(error?.message || "Failed to process email notification");
+    throw new Error(error?.message || "Failed to process email notification.");
   }
-}
+};
 
-export { sendEmail }
+export { sendEmail };
 
